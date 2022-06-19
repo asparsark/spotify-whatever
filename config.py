017285cde06d76cf7c5308b172f4f274100c88b9
asparsark <kaspars.udrovskis@gmail.com>
2022-06-19 23:56:13 +0200

    config stuffs

---
+++ config.py
@@ -0,0 +1,22 @@
+import json
+
+cfg_json = 'config.json'
+
+def load(cfg_path=cfg_json):
+    with open(cfg_path, 'r') as j:
+        cfg = json.load(j)
+
+    return cfg
+
+
+def update(payload: dict, cfg_path=cfg_json):
+    cfg = load()
+
+    for i in payload:
+        if i in cfg:
+            cfg[i] = payload[i]
+
+    with open(cfg_path, 'w') as j:
+        json.dump(cfg, j, indent=4)
+
+    return load()
